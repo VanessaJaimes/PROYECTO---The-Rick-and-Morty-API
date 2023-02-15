21ff25cb8fea7beb8a3a9dde9a71623b97fc30cc
juans <juansmgb@gmail.com>
2023-02-15 17:26:06 -0500

    login y registro

--- main.js
+++ main.js
@@ -1,36 +1,46 @@
-const usuarios= [
+const main = Vue.createApp({
+  data() {
+    return {
+     user: "",
+     clave: "",
+     gmail: "",
+     loginExitoso:false,
+     registroExitoso: false,
+     usuarios: [],
+
+
+  };
+},
+methods: {
+  login() {
+   let usuario = this.usuarios.find(u => u.user === this.user && u.clave === this.clave);
+    if(usuario){
+      this.loginExitoso = true;
+      alert("Login Exitoso");
+    }else{
+      this.loginExitoso = false;
+      alert("Usuario o Clave Incorrectos")
+    }
+  },
   
 
-];
+  registro(){
+    if(this.user && this.clave && this.gmail){
+      this.usuarios.push({
+         user: this.user,
+         clave: this.clave,
+         gmail: this.gmail
+      });
+      alert("Usuario Registrado Exitosamente");
+      this.registroExitoso = true;
+    }else{
+      alert("Debe completar todos los campos");
+      this.registroExitoso = false;
+    }
+  },
 
 
-  const main = Vue.createApp({
-      data() {
-        return {
-         user: "",
-         clave: "",
-         gmail: "",
-         loginExitoso:false,
-         registroExitoso: false,
+}
+});
 
-  
-         
-          
-        }
-      },
-      methods: {
-        login() {
-         alert("logn")
-        },
-        
-  
-        registro(){
-          alert("registro")
-        },
-  
-  
-      }
- 
-    });
-  
-    main.mount(".main");
+main.mount(".main");
